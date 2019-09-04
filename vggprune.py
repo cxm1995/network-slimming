8211897ced5f8aa6c8d87d6fb76bae06dd54cb11
cxm1995 <742318674@qq.com>
2019-09-04 15:02:13 +0800

    Update vggprune.py
    to remove the error:
    mask = weight_copy.gt(thre).float().cuda()
    RuntimeError: Expected object of type torch.cuda.FloatTensor but found type torch.FloatTensor for argument #2 'other'

--- vggprune.py
+++ vggprune.py
@@ -69,7 +69,7 @@ cfg = []
 cfg_mask = []
 for k, m in enumerate(model.modules()):
     if isinstance(m, nn.BatchNorm2d):
-        weight_copy = m.weight.data.abs().clone()
+        weight_copy = m.weight.data.abs().clone().cpu()
         mask = weight_copy.gt(thre).float().cuda()
         pruned = pruned + mask.shape[0] - torch.sum(mask)
         m.weight.data.mul_(mask)
